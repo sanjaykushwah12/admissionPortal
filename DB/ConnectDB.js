4acb42b4d942a59ff3390c720e7442b89cd14e7b
sanjay kushwah <sanjaykushwah2020@gmail.com>
2023-05-12 15:46:47 +0530

    dt

--- DB/ConnectDB.js
+++ DB/ConnectDB.js
@@ -1,8 +1,10 @@
 const mongoose =require('mongoose')
+const url = "'mongodb://0.0.0.0:27017/AdmissionPortal'"
+const live_uri = "mongodb+srv://sanjaykushwah2020:AEIdHmsJHcaa0jnj@admissionportal.zhqm9tr.mongodb.net/?retryWrites=true&w=majority"
 
 
 const connectDB =()=>{
-      return mongoose.connect('mongodb://0.0.0.0:27017/AdmissionPortal')
+      return mongoose.connect(live_uri)
 
       .then(()=>{
         console.log('connection successfully')
